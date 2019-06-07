52606193b314f8c0ffdedd140f8dd6cdd522c600
Anton Durandin <enitefall81@gmail.com>
2019-06-06 23:10:54 -0700

    Revert "BoxIt finished"
    This reverts commit 3124cd47839e9e8783e9ed8e0ccfa44c28270cb4.

--- box_it_script/test.js
+++ box_it_script/test.js
@@ -1,6 +1,3 @@
-
-const arg = process.argv.slice(2);
-
 let draw = {
     drawLine: function (lineLength) {
         let result = '';
@@ -11,78 +8,82 @@ let draw = {
         return result;
     },
 
-    drawSpaces: function (spaceLength) {
-        let result = '';
-        let line = ' ';
-        for (let element = 0; element < spaceLength; element += 1) {
-            result += line;
-        }
-        return result;
-    },
-
     drawTopBorder: function (lineLength) {
         //debugger;
         let justLine = this.drawLine(lineLength);
         let topBorder = `┏${justLine}┓\n`;
-        // console.log(topBorder);
+        console.log(topBorder);
         return topBorder;
     },
 
     drawMiddleBorder: function (lineLength) {
         let justLine = this.drawLine(lineLength);
         let drawMiddleBorder = `┣${justLine}┫\n`;
-        //  console.log(drawMiddleBorder);
+        console.log(drawMiddleBorder);
         return drawMiddleBorder;
     },
 
     drawLowBorder: function (lineLength) {
         let justLine = this.drawLine(lineLength);
         let lowBorder = `┗${justLine}┛\n`;
-        // console.log(lowBorder);
+        console.log(lowBorder);
         return lowBorder;
     },
 
-    drawBarsAround: function (string, spacing) {
-        let result = '';
-        if (string.length !== spacing) {
-debugger;
-            
-            let resultPadding = this.drawSpaces(spacing)
-            result += `┃${string}${resultPadding}┃\n`;
-        };
-        //  console.log(result);
-        return result;
+    drawBarsAround: function (string) {
+    let result = "";
+    result = `┃${string}┃\n`;
+        console.log(result);
+    return result;    
     },
-
 
     calculateLongest: function (array) { /// finding the longest array
-        let longest = array.concat().sort(function (a, b) { return b.length - a.length; })[0];
-        return longest.length;
+    let longest = array.sort(function (a, b) { return b.length - a.length; })[0];
+    return longest.length;
     },
 
+    boxIt: function (arrayOfStrings) {
+let result = "";
+let lengthNumber = this.calculateLongest(arrayOfStrings);   //counting number of lines to draw ━     
+arrayOfStrings.array.forEach(element, index => {
+if (element[index] === 0) {
+result+= this.drawTopBorder(lengthNumber) + this.drawBarsAround(element)
+} else if (element[index] !== arrayOfStrings.length - 1) {
+result+= this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(element)
+} else {
+result+= this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(element) + this.drawLowBorder(lentghNumber)    
+}  
+});
+    },
 
-    boxItFull: function (arrayOfStrings) {
-        let result = "";
-        let lineToAddToBarsAround = 0;
-        let lengthNumber = this.calculateLongest(arrayOfStrings);   //counting number of lines to draw ━     
-        for (let element = 0; element < arrayOfStrings.length; element += 1) {
-            //debugger;
-            elementOfArray = arrayOfStrings[element].length;
-            lineToAddToBarsAround = lengthNumber - elementOfArray;
-            if (arrayOfStrings.length===1) {
-                result = this.drawTopBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element]) + this.drawLowBorder(lengthNumber);
-            } else if ([element] == 0) {
-                result = this.drawTopBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element], lineToAddToBarsAround);
-            } else if (element !== (parseInt(arrayOfStrings.length) - 1)) {
-                result += this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element], lineToAddToBarsAround);
-            } else {
-                result += this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element], lineToAddToBarsAround) + this.drawLowBorder(lengthNumber);
-            }
-        }
+boxItFull: function (arrayOfStrings) {
+    let result = "";
+    let lengthNumber = this.calculateLongest(arrayOfStrings);   //counting number of lines to draw ━     
+    for (let element = 0; element< arrayOfStrings.length; element+=1) {
+    if (arrayOfStrings[element] === 0) {
+    result+= this.drawTopBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element])
+    } else if (arrayOfStrings[element] !== arrayOfStrings.length - 1) {
+    result+= this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element])
+    } else {
+    result+= this.drawMiddleBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[element]) + this.drawLowBorder(lentghNumber)    
+    }
+}  
+    
 
-        return result;
+// 
+/*if (arrayOfStrings.length ===1) {
+    result = this.drawTopBorder(lengthNumber) + this.drawBarsAround(arrayOfStrings[0]) + this.drawLowBorder(lengthNumber);
+    } else {
+    let lengthNumber = this.calculateLongest(arrayOfStrings);/// longest name to make boxes as long as the bisggest array element to fit them    
+    for (let names = 0; names < arrayOfStrings.length; names+=1) {
+    result = this.drawTopBorder(lengthNumber)    
+    }    
+    }
+    */
+//console.log(result);
+return result;
     },
 }
 
-
-console.log(draw.boxItFull(arg));
+//console.log(draw.drawTopBorder(4)) ;
+console.log(draw.boxItFull(['Jon Snow', 'Cersei Lannister']));
