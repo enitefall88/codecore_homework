1b3535226ad628a058954eda2f5847851bb74e5d
Anton Durandin <enitefall81@gmail.com>
2019-06-16 21:40:02 -0700

    Almost finish all functions, but without writing to a file

--- todo_cli/todo_cli.js
+++ todo_cli/todo_cli.js
@@ -6,28 +6,83 @@ const readlineInstance = readline.createInterface({
 });
 
 let todoList = {
+todos: [],
+    mainMenu: function () {
+        console.log('(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit ');
+        readlineInstance.question('Choose your destiny...', (answer) => {
+            switch (answer.trim()) {
+                case 'v':
+                    this.viewTodo();
+                    break;
+                case 'n':
+                    this.addTodo();
+                    break;
+                case 'cX':
+                    this.toggleCompleted();
+                    break;
+                case 'dX':
+                    this.deleteTodo();
+                    break;
+                case 'q':
+                    quitTodo();
+                    break;
+                default:
+                    console.log("Enter a valid option");
+                    this.mainMenu();
+            }
+        })
+    },
 
-mainMenu: function () {
-console.log('\x1b[34m(v) View | (n) New | (cX) Complete | (dX) Delete | (q) Quit \x1b[0m');
-releaseEvents.question('Choose your destiny...',(answer)=> {
-  switch(answer.trim()) {
-  case 'v':
-      this.viewTodo();
-      break;
-      case  'n':
-          this.addTodo();
-          break;
-          case 'cX':
-              this.toggleCompleted();
-              break;
-              case 'dX':
-              this.deleteTodo();
-              break;
-              case 'q':
-                  quitTodo();
-                  break;  
-  }  
-}
-},   
+    viewTodo: function () {
+if (this.todos.length===0) {
+console.log('Todo-list is empty')
+};
+this.todos.forEach(element => {
+    if (element.completed=== true) {
+console.log(`[✓] ${element.todoText}`)
+} else {
+    console.log(`[ ] ${element.todoText}`)  
+}  
+});
+this.mainMenu();
+    },
+
+addTodo: function () {
+console.log('Please, type in your todo');
+readlineInstance.question('What?', answer => {
+    if (answer) {
+        this.todos.push({
+        todoText: answer,
+        completed: false,   
+       });
+       this.mainMenu()
+    };
+})
     
+},
+
+toggleCompleted: function (position) {
+readlineInstance.question ('please choose your todo', answer => {
+    this.todos[answer] = !this.todos[answer];
+    this.mainMenu();
+})
+
+},
+
+deleteTodo: function () {
+ readlineInstance.question('Enter a number to delete a todo', answer=> {
+ this.todos.splice(answer-1,1);    
+this.mainMenu(); 
+})   
+},
+
+quitTodo: function () {
+ readlineInstance.close();
+ console.log('Have a nice day!');
+ }
+
+
+
 }
+
+todoList.mainMenu();
